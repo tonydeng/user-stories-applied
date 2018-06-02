f84ebe4a5cb86227341b45ff8a71f80251d9641e
Travis CI <travis@travis-ci.org>
2018-06-02 18:21:27 +0800

    :wrench: chore(1.0): 调整手工发布到github的脚本
    refs #1
    
    Signed-off-by: Travis CI <travis@travis-ci.org>

--- deploy-to-github.sh
+++ deploy-to-github.sh
@@ -1,26 +1,38 @@
 #!/bin/sh
 
 setup_git(){
-   git config --global user.name "Travis CI"
-   git config --global user.email "travis@travis-ci.org"
+   git config --global user.name "Tony Deng"
+   git config --global user.email "wolf.deng@gmail.com"
+   git remote add origin-pages https://github.com/tonydeng/user-stories-applied.git > /dev/null 2>&1
 }
 
+build_gitbook(){
+    gitbook install
+    gitbook build
+}
 
 commit_website_files(){
     git checkout -b gh-pages
-    git pull origin gh-pages --rebase
+    git pull origin-pages gh-pages --rebase
     cp -R _book/* .
     git clean -fx node_modules
     git clean -fx _book
     git add .
-    git commit -a -m "Travis build: $TRAVIS_BUILD_NUMBER"
+    git commit -a -m "deploy books"
 }
 
 upload_files(){
-    git remote add origin-pages https://${GH_TOKEN}@github.com/tonydeng/user-stories-applied.git > /dev/null 2>&1
     git push --quiet --set-upstream origin-pages gh-pages
 }
 
+clean_env(){
+    git remote rm origin-pages
+    git checkout master
+    git branch -D gh-pages
+}
+
 setup_git
+build_gitbook
 commit_website_files
 upload_files
+clean_env
